e437e500c89669091ec9a24562acadcc3148cc5d
IvannaGotsova <ivannagotsova@outlook.com>
2024-06-29 16:44:29 +0300

    Basic App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
+import Navigation from './Navigation'
+import Body from './Body'
+import Footer from './Footer'
 
 function App() {
 
   return (
     <>
-      
+      <Header /> <br /><br />
+      <Navigation /> <br /><br />
+      <Body /> <br /><br />
+      <Footer />
     </>
   )
 }
