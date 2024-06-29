0dd9008d956ae70c17df2e9df0e7348478e3fc33
IvannaGotsova <ivannagotsova@outlook.com>
2024-06-29 17:31:22 +0300

    Added nested components

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,7 +1,17 @@
+import Breakfast from "./Breakfast"
+import Desert from "./Desertsjsx"
+import Dinner from "./Dinner"
+import Lunch from "./Lunch"
+import Salad from "./Salad"
+import Soup from "./Soup"
+
 function Body () {
 
-    return "Body"
-
+    return 
+    <>
+      <Breakfast /> <Lunch /> <Dinner /> <br /><br />
+      <Salad /> <Soup /> <Desert />
+    </>
 }
 
 export default Body
