a2b0c52278ac04a38eb663cc44fd960db8f9b390
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-02 11:59:28 +0300

    Modified to show json file

--- src/components/Salad.jsx
+++ src/components/Salad.jsx
@@ -1,6 +1,25 @@
+import salad from './db/salad.json'
+
 function Salad () {
 
-    return "Salad"
+    
+    return (
+        <>
+        <h1>Salad</h1>
+        <hr />
+        {salad.map(item => {
+          return (
+             <div>
+                <h3>{item.name}</h3>
+                <p key={item.id}>{item.id} - {item.type}</p>
+                <img src={item.image} alt="" />
+                <p>{item.ingredients}</p>
+                <p>{item.howTo}</p>
+             </div>
+           )
+        })}
+      </>
+    )
 
 }
 
