6256f111fb135819963f7f84769d4c11c80caad5
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-02 12:00:07 +0300

    Modified to show json file

--- src/components/Dinner.jsx
+++ src/components/Dinner.jsx
@@ -1,6 +1,25 @@
+import dinner from './db/dinner.json'
+
 function Dinner () {
 
-    return "Dinner"
+    
+    return (
+        <>
+        <h1>Dinner</h1>
+        <hr />
+        {dinner.map(item => {
+          return (
+             <div>
+                <h3>{item.name}</h3>
+                <p key={item.id}>{item.id} - {item.type}</p>
+                <img src={item.image} alt="" />
+                <p>{item.ingredients}</p>
+                <p>{item.howTo}</p>
+             </div>
+           )
+        })}
+      </>
+    )
 
 }
 
