7eb517285b2acbe9c4a88848993c2bbc79bd1a64
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-02 11:59:54 +0300

    Modified to show json file

--- src/components/Lunch.jsx
+++ src/components/Lunch.jsx
@@ -1,6 +1,25 @@
+import lunch from './db/lunch.json'
+
 function Lunch () {
 
-    return "Lunch"
+    
+    return (
+        <>
+        <h1>Lunch</h1>
+        <hr />
+        {lunch.map(item => {
+          return (
+             <div>
+                <h3>{item.name}</h3>
+                <p key={item.id}>{item.id} - {item.type}</p>
+                <img src={item.image} alt="" />
+                <p>{item.ingredients}</p>
+                <p>{item.howTo}</p>
+             </div>
+           )
+        })}
+      </>
+    )
 
 }
 
