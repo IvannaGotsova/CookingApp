917dcabebc0b8ea0e34886284dfd97f2fa798c70
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-02 12:00:20 +0300

    Modified to show json file

--- src/components/Desert.jsx
+++ src/components/Desert.jsx
@@ -1,6 +1,25 @@
+import desert from './db/desert.json'
+
 function Desert () {
 
-    return "Desert"
+    
+    return (
+        <>
+        <h1>Desert</h1>
+        <hr />
+        {desert.map(item => {
+          return (
+             <div>
+                <h3>{item.name}</h3>
+                <p key={item.id}>{item.id} - {item.type}</p>
+                <img src={item.image} alt="" />
+                <p>{item.ingredients}</p>
+                <p>{item.howTo}</p>
+             </div>
+           )
+        })}
+      </>
+    )
 
 }
 
