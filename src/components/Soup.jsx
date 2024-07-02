b5b0665c815f6e5b1f202b0dd1bd448fd829d742
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-02 11:59:21 +0300

    Modified to show json file

--- src/components/Soup.jsx
+++ src/components/Soup.jsx
@@ -1,6 +1,25 @@
+import soup from './db/soup.json'
+
 function Soup () {
 
-    return "Soup"
+    
+    return (
+        <>
+        <h1>Soup</h1>
+        <hr />
+        {soup.map(item => {
+          return (
+             <div>
+                <h3>{item.name}</h3>
+                <p key={item.id}>{item.id} - {item.type}</p>
+                <img src={item.image} alt="" />
+                <p>{item.ingredients}</p>
+                <p>{item.howTo}</p>
+             </div>
+           )
+        })}
+      </>
+    )
 
 }
 
