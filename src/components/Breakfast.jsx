cdae5a895340bae135410cfc0388245aa3fcacab
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-02 12:00:27 +0300

    Modified to show json file

--- src/components/Breakfast.jsx
+++ src/components/Breakfast.jsx
@@ -1,6 +1,25 @@
+import breakfast from './db/breakfast.json'
+
 function Breakfast () {
 
-    return "Breakfast"
+    
+    return (
+        <>
+        <h1>Breakfast</h1>
+        <hr />
+        {breakfast.map(item => {
+          return (
+             <div>
+                <h3>{item.name}</h3>
+                <p key={item.id}>{item.id} - {item.type}</p>
+                <img src={item.image} alt="" />
+                <p>{item.ingredients}</p>
+                <p>{item.howTo}</p>
+             </div>
+           )
+        })}
+      </>
+    )
 
 }
 
