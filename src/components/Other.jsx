c3319d574046498f6188c6b208f62b25b5f4105f
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-04 12:39:13 +0300

    Added Receipt tag

--- src/components/Other.jsx
+++ src/components/Other.jsx
@@ -1,4 +1,5 @@
 import other from './db/other.json'
+import Receipt from './Receipt'
 
 function Other () {
 
@@ -7,17 +8,7 @@ function Other () {
         <>
         <h1>Others</h1>
         <hr />
-        {other.map(item => {
-          return (
-             <div>
-                <h3>{item.name}</h3>
-                <p key={item.id}>{item.id} - {item.type}</p>
-                <img src={item.image} alt="" />
-                <p>{item.ingredients}</p>
-                <p>{item.howTo}</p>
-             </div>
-           )
-        })}
+        <Receipt collection = {other}/>
       </>
     )
 
