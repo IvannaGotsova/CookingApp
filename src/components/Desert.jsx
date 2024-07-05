533a8d22c7ca6abc50aa839dbbb033f377e9d347
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-05 20:37:32 +0300

    Added new path for import

--- src/components/Desert.jsx
+++ src/components/Desert.jsx
@@ -1,12 +1,12 @@
 import desert from './db/desert.json'
 import Receipt from './Receipt'
-import styles from './Receipt.module.css'
+import styles from '../css/Receipt.module.css'
 
 function Desert () {
 
     
     return (
-        <div style={{backgroundColor: "gray"}}>
+      <div className={styles.desertStyle}>
         <h1>Desert</h1>
         <hr />
         <Receipt collection = {desert}/>
