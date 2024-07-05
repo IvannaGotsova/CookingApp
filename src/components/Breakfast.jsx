bba40484b87012ef8e3e0bbcab9cf569f3d68c7a
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-05 19:09:48 +0300

    Added CSS module styling

--- src/components/Breakfast.jsx
+++ src/components/Breakfast.jsx
@@ -1,11 +1,12 @@
 import breakfast from './db/breakfast.json'
 import Receipt from './Receipt'
+import styles from './Receipt.module.css'
 
 function Breakfast () {
 
     
     return (
-      <div style={{backgroundColor: "gray"}}>
+      <div className={styles.breakfastStyle}>
         <h1 >Breakfast</h1>
         <hr />
         <Receipt  collection = {breakfast} />
