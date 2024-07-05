3c7edb075d78a5ce73d23585e8f4deb5292eec3d
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-05 20:37:56 +0300

    Added new path for import

--- src/components/Salad.jsx
+++ src/components/Salad.jsx
@@ -1,12 +1,12 @@
 import salad from './db/salad.json'
 import Receipt from './Receipt'
-import styles from './Receipt.module.css'
+import styles from '../css/Receipt.module.css'
 
 function Salad () {
 
     
     return (
-      <div style={{backgroundColor: "gray"}}>
+      <div className={styles.saladStyle}>
         <h1>Salad</h1>
         <hr />
         <Receipt collection = {salad}/>
