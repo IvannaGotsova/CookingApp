8d5e9063550ec651c0a221265fd27d6f3a21a9d9
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-07 17:01:31 +0300

    Added all elements in div

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -16,37 +16,58 @@ function Body () {
       <div>
         <BrowserRouter>
           <Routes>
-            <Route path="breakfast" element={<Breakfast />} />
-            <Route path="lunch" element={<Lunch />} />
-            <Route path="dinner" element={<Dinner />} />
-            <Route path="salad" element={<Salad />} />
-            <Route path="soup" element={<Soup />} />
-            <Route path="desert" element={<Desert />} />
-            <Route path="other" element={<Other />} />
+            <Route path="/">
+              <Route path="breakfast" element={<Breakfast />} />
+              <Route path="lunch" element={<Lunch />} />
+              <Route path="dinner" element={<Dinner />} />
+              <Route path="salad" element={<Salad />} />
+              <Route path="soup" element={<Soup />} />
+              <Route path="desert" element={<Desert />} />
+              <Route path="other" element={<Other />} />
+            </Route>
           </Routes>
         </BrowserRouter>
+        
         <ul className={styles.removedBullets}>
-            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
-            <li><a href="Breakfast">Breakfast</a></li>
-            <br />
-            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
-            <li><a href="Lunch">Lunch</a></li>
-            <br />
-            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
-            <li><a href="Dinner">Dinner</a></li>
-            <br />
-            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
-            <li><a href="Salad">Salad</a></li>
-            <br />
-            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
-            <li><a href="Soup">Soup</a></li>
-            <br />
-            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
-            <li><a href="Desert">Desert</a></li>
-            <br />
-            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
-            <li><a href="Other">Other</a></li>
+          <div className={styles.addedOrder}>
+            <div>
+              <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+              <li><a href="Breakfast">Breakfast</a></li>
+              <br />
+            </div>
+            <div>
+              <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+              <li><a href="Lunch">Lunch</a></li>
+              <br />
+            </div>
+            <div>
+              <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+              <li><a href="Dinner">Dinner</a></li>
+              <br />
+            </div>
+            <div>
+              <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+              <li><a href="Salad">Salad</a></li>
+              <br />
+            </div>
+            <div>
+              <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+              <li><a href="Soup">Soup</a></li>
+              <br />
+            </div>
+            <div>
+              <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+              <li><a href="Desert">Desert</a></li>
+              <br />
+            </div>
+            <div>
+              <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+              <li><a href="Other">Other</a></li>
+              <br />
+            </div>
+          </div>
         </ul>
+        
       </div>
     </>
     )
