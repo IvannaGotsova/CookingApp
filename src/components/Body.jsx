c6602a595158f0bb00539d3b3795268cf657ce8f
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-07 11:10:48 +0300

    Added Routes and links

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,20 +5,47 @@ import Lunch from "./Lunch"
 import Other from "./Other"
 import Salad from "./Salad"
 import Soup from "./Soup"
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styles from '../css/Body.module.css'
 
 function Body () {
 
     return  (
     <>
-      <Breakfast /> 
-      <Lunch /> 
-      <Dinner /> 
-      <Salad /> 
-      <Soup /> 
-      <Desert />
-      <Other />
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="breakfast" element={<Breakfast />} />
+            <Route path="lunch" element={<Lunch />} />
+            <Route path="dinner" element={<Dinner />} />
+            <Route path="salad" element={<Salad />} />
+            <Route path="soup" element={<Soup />} />
+            <Route path="desert" element={<Desert />} />
+            <Route path="other" element={<Other />} />
+          </Routes>
+        </BrowserRouter>
+        <ul className={styles.removedBullets}>
+            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+            <li><a href="Breakfast">Breakfast</a></li>
+            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+            <li><a href="Lunch">Lunch</a></li>
+            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+            <li><a href="Dinner">Dinner</a></li>
+            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+            <li><a href="Salad">Salad</a></li>
+            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+            <li><a href="Soup">Soup</a></li>
+            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+            <li><a href="Desert">Desert</a></li>
+            <img className={styles.imageStyle} src="src\\assets\\Picture.jpg" alt="" />
+            <li><a href="Other">Other</a></li>
+        </ul>
+      </div>
     </>
     )
 }
 
 export default Body
+
+
