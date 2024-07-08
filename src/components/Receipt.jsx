b4cf02f22a374cd2a5f8344e4158185b9d6ac04c
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-08 15:24:53 +0300

    Added buttons for ingredients and how to make

--- src/components/Receipt.jsx
+++ src/components/Receipt.jsx
@@ -1,8 +1,7 @@
 import styles from '../css/Receipt.module.css'
 
 
-function Receipt (props) {
-
+function Receipt (props) {    
     
     return (
         <>
@@ -12,6 +11,9 @@ function Receipt (props) {
                 <h3>{item.name}</h3>
                 <p key={item.id} id={item.id}>{item.id} - {item.type}</p>
                 <img className={styles.imageStyle} src={item.image} alt="" />
+                <br />
+                <button>Ingredients</button>
+                <button>How to Make</button>
                 <p>{item.ingredients}</p>
                 <p>{item.howTo}</p>
              </div>
