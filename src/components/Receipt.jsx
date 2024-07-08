61482b868fe5b8191b266b3880b3950d76017dd6
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-08 15:49:03 +0300

    Added popup trigger

--- src/components/Receipt.jsx
+++ src/components/Receipt.jsx
@@ -1,3 +1,4 @@
+import Popup from 'reactjs-popup';
 import styles from '../css/Receipt.module.css'
 
 
@@ -12,10 +13,16 @@ function Receipt (props) {
                 <p key={item.id} id={item.id}>{item.id} - {item.type}</p>
                 <img className={styles.imageStyle} src={item.image} alt="" />
                 <br />
-                <button className={styles.buttonStyle}>Ingredients</button>
-                <button className={styles.buttonStyle}>How to Make</button>
+                <Popup trigger=
+                {<button className={styles.buttonStyle}>Ingredients</button>}
+                position="left center">
                 <p>{item.ingredients}</p>
+                </Popup>
+                <Popup trigger=
+                {<button className={styles.buttonStyle}>How to Make</button>}
+                position="right center">
                 <p>{item.howTo}</p>
+                </Popup>             
              </div>
            )
         })}
