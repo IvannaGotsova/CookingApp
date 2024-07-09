8ebd25f454f64300a9ec815862d46c3fbd803927
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-09 12:28:08 +0300

    Removed unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,6 @@ import Header from './components/Header'
 import Navigation from './components/Navigation'
 import Body from './components/Body'
 import Footer from './components/Footer'
-import Breakfast from './components/Breakfast'
-import Salad from './components/Salad'
-import Desert from './components/Desert'
-import Lunch from './components/Lunch'
-import Dinner from './components/Dinner'
-import Soup from './components/Soup'
 
 function App() {
 
