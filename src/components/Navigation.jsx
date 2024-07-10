9e493386fe6810a0746586d83bd314e8927e3c40
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-10 13:49:22 +0300

    Updated Navigation component with basic content

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,23 @@
+import styles from '../css/Body.module.css'
+
 function Navigation () {
 
-    return "Navigation"
-
+    return (
+        <>
+            <div className={styles.displayStyle}>
+                <ul className={styles.removedBullets}>
+                    <li><a href="/">Home  </a></li> 
+                    <li><a href="Breakfast">Breakfast  </a></li>
+                    <li><a href="Lunch">Lunch  </a></li>
+                    <li><a href="Dinner">Dinner  </a></li>
+                    <li><a href="Salad">Salad  </a></li>
+                    <li><a href="Soup">Soup  </a></li>
+                    <li><a href="Desert">Desert  </a></li>
+                    <li><a href="Other">Other  </a></li>
+                </ul>                
+            </div>
+        </>
+    )
 }
 
 export default Navigation
