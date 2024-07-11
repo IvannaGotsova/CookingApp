b399ffe0115337693ac4c7b1737f40e254e76bd0
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-11 13:21:50 +0300

    Added heading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import styles from '../css/Body.module.css'
+
+
 function Header () {
 
-    return "Header"
-
+    return (
+        <>
+            <h1 className={styles.headingStyle}>COOKING APP</h1>
+        </>
+    )
 }
 
 export default Header
