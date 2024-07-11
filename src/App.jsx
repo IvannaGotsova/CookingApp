ee4d7db4b7aa1eada9166cfa39a888273b27a839
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-11 13:21:38 +0300

    Added Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Navigation from './components/Navigation'
+import Search from './components/Search'
 import Body from './components/Body'
 import Footer from './components/Footer'
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header /> <br /><br />
+      <Search /> <br /><br />
       <Navigation /> <br /><br />
       <Body /><br /><br />
       <Footer />
