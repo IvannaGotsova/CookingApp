abc478a12a44d5b82332bceb1d2872617e293aba
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-16 22:24:16 +0300

    Added Details for salad

---
+++ src/components/SaladDetails.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import salad from './db/salad.json'
+
+ function SaladDetails () {
+    
+    return (
+        <></>
+    )
+}
+
+export default SaladDetails
