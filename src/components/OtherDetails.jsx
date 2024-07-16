de0f720eb96ff7c7c7bf26f9a91c934f428909be
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-16 22:24:09 +0300

    Added Details for other

---
+++ src/components/OtherDetails.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import other from './db/other.json'
+
+function OtherDetails () {
+    
+    return (
+        <></>
+    )
+}
+
+export default OtherDetails
