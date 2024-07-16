a60027acf673fde5344fecb110464ac8709c8506
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-16 22:24:03 +0300

    Added Details for lunch

---
+++ src/components/LunchDetails.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import lunch from './db/lunch.json'
+
+function LunchDetails () {
+    
+    return (
+        <></>
+    )
+}
+
+export default LunchDetails
