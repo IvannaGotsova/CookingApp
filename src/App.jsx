c07efd16ca111f94a8f99f5b25ae8d669ed9a505
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-16 22:23:06 +0300

    Added routes for details

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,13 @@ import Other from "./components/Other"
 import Salad from "./components/Salad"
 import Soup from "./components/Soup"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import BreakfastDetails from './components/BreakfastDetails'
+import LunchDetails from './components/LunchDetails'
+import DinnerDetails from './components/DinnerDetails'
+import SaladDetails from './components/SaladDetails'
+import SoupDetails from './components/SoupDetails'
+import DesertDetails from './components/DesertDetails'
+import OtherDetails from './components/OtherDetails'
 function App() {
 
   return (
@@ -25,12 +31,19 @@ function App() {
         <Routes>
             <Route path="/" element={<Body />} />
             <Route path="breakfast" element={<Breakfast />} />
+            <Route path="breakfast/:id" element={<BreakfastDetails />} />
             <Route path="lunch" element={<Lunch />} />
+            <Route path="lunch/:id" element={<LunchDetails />} />
             <Route path="dinner" element={<Dinner />} />
+            <Route path="dinner/:id" element={<DinnerDetails />} />
             <Route path="salad" element={<Salad />} />
+            <Route path="salad/:id" element={<SaladDetails />} />
             <Route path="soup" element={<Soup />} />
+            <Route path="soup/:id" element={<SoupDetails />} />
             <Route path="desert" element={<Desert />} />
+            <Route path="desert/:id" element={<DesertDetails />} />
             <Route path="other" element={<Other />} />
+            <Route path="other/:id" element={<OtherDetails />} />
         </Routes>
       </BrowserRouter><br /><br />
 
