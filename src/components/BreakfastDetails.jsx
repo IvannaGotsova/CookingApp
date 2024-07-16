a62c5578628e8131f4e4c05fd38494b68f740a3e
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-16 22:25:51 +0300

    Sync

--- src/components/BreakfastDetails.jsx
+++ src/components/BreakfastDetails.jsx
@@ -5,6 +5,7 @@ import breakfast from './db/breakfast.json'
 function BreakfastDetails () {
 
     const { id } = useParams();
+    const [breakfastDetails, setBreakfastDetails] = useState();
 
     async function loadBreakfastDetails (jsonBreakfast) {
         const resultdetails = await fetch(jsonBreakfast);
