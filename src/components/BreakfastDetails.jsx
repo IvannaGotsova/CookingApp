b21c28ea536ff386c0aa3aff50caac9f19ca6254
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-16 22:25:34 +0300

    Added params

--- src/components/BreakfastDetails.jsx
+++ src/components/BreakfastDetails.jsx
@@ -3,10 +3,21 @@ import { useParams } from 'react-router-dom';
 import breakfast from './db/breakfast.json'
 
 function BreakfastDetails () {
-   
+
+    const { id } = useParams();
+
+    async function loadBreakfastDetails (jsonBreakfast) {
+        const resultdetails = await fetch(jsonBreakfast);
+        return resultdetails;
+      }
+    
+    loadBreakfastDetails(breakfast)
+      
+    
     return (
         <></>
     )
+
 }
 
 export default BreakfastDetails
