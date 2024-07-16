923c60559753358bc6365aee328c187e9e5733da
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-16 22:23:40 +0300

    Added Details for desert

---
+++ src/components/DesertDetails.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import desert from './db/desert.json'
+
+function DesertDetails () {
+    
+    return (
+        <></>
+    )
+}
+
+export default DesertDetails
