7ab4b114c4e90d3a2a696e45f3d7080dff31b556
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-16 22:23:31 +0300

    Added Details for breakfast

---
+++ src/components/BreakfastDetails.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import breakfast from './db/breakfast.json'
+
+function BreakfastDetails () {
+   
+    return (
+        <></>
+    )
+}
+
+export default BreakfastDetails
