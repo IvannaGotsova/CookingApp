5711458b09d54e0fea4eaa952b21817fd14eb766
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-17 13:40:23 +0300

    Modified Details component

--- src/components/BreakfastDetails.jsx
+++ src/components/BreakfastDetails.jsx
@@ -1,22 +1,42 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import breakfast from './db/breakfast.json'
+import styles from '../css/Details.module.css'
 
 function BreakfastDetails () {
 
     const { id } = useParams();
-    const [breakfastDetails, setBreakfastDetails] = useState();
+    const [breakfastDetails, setBreakfastDetails] = useState(null);
 
-    async function loadBreakfastDetails (jsonBreakfast) {
-        const resultdetails = await fetch(jsonBreakfast);
-        return resultdetails;
-      }
-    
-    loadBreakfastDetails(breakfast)
+    useEffect(() => {
+        fetch({breakfast})
+            .then(response => {
+                if (response.statusText == 'No Content') {
+                    console.log('No Content')
+                    return;
+                }
+
+                return response;
+            })
+            .then(result => setBreakfastDetails(result));
+    }, [id]);
+
       
     
     return (
-        <></>
+        <div className={styles.detailsStyle}>
+            <h4>{breakfast[id - 1].type}</h4>
+            <h1>{breakfast[id - 1].name}</h1>
+            <img src={breakfast[id - 1].image} alt="" />
+            <h3>INGREDIENTS</h3>
+            <ul>
+                {breakfast[id - 1].ingredients.map((ingredient) => (
+                  <li key={ingredient}>{ingredient}</li>
+                ))}
+            </ul>
+            <h3>HOW TO MAKE</h3>
+            <p>{breakfast[id - 1].howTo}</p>
+        </div>
     )
 
 }
