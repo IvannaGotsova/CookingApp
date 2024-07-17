b7ad5bb9e44413a56bf4b0a96b7e0e61a862db45
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-17 13:40:45 +0300

    Modified Details component

--- src/components/SaladDetails.jsx
+++ src/components/SaladDetails.jsx
@@ -1,11 +1,42 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import salad from './db/salad.json'
+import styles from '../css/Details.module.css'
 
  function SaladDetails () {
     
+    const { id } = useParams();
+    const [saladDetails, setSaladDetails] = useState(null);
+
+    useEffect(() => {
+        fetch({salad})
+            .then(response => {
+                if (response.statusText == 'No Content') {
+                    console.log('No Content')
+                    return;
+                }
+
+                return response;
+            })
+            .then(result => setSaladDetails(result));
+    }, [id]);
+
+      
+    
     return (
-        <></>
+        <div className={styles.detailsStyle}>
+            <h4>{salad[id - 1].type}</h4>
+            <h1>{salad[id - 1].name}</h1>
+            <img src={salad[id - 1].image} alt="" />
+            <h3>INGREDIENTS</h3>
+            <ul>
+                {salad[id - 1].ingredients.map((ingredient) => (
+                  <li key={ingredient}>{ingredient}</li>
+                ))}
+            </ul>
+            <h3>HOW TO MAKE</h3>
+            <p>{salad[id - 1].howTo}</p>
+        </div>
     )
 }
 
