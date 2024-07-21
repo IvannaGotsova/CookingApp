891acfdf819f8faa6b17df58f7fd238cf524194a
IvannaGotsova <ivannagotsova@outlook.com>
2024-07-21 17:09:04 +0300

    Added imports

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,12 @@
 import styles from '../css/Body.module.css'
+import React, { useState } from 'react'
+import breakfast from './db/breakfast.json'
+import desert from './db/desert.json'
+import dinner from './db/dinner.json'
+import lunch from './db/lunch.json'
+import other from './db/other.json'
+import salad from './db/salad.json'
+import soup from './db/soup.json'
 
 function Search () {
 
